a989cb589a6fb16375b44156adff5f794fdee68c
root <root@DESKTOP-4N8IPC8>
2023-04-02 04:21:07 +0300

    degisiklikler yapildi.

--- Egitim/src/deneme.java
+++ Egitim/src/deneme.java
@@ -1,34 +1,16 @@
-	import java.util.Scanner;
+public class deneme {
+    public static void main(String[] args) {
 
-	public class deneme {
-	            public static void main(String[] args) {
-	                Scanner scanner = new Scanner(System.in);
-	                int number1,number2;
-	                System.out.print("Please enter the first number: ");
-	                number1 = scanner.nextInt();
-	                System.out.print("Please enter the second number: ");
-	                number2 = scanner.nextInt();
-
-	                int i = (number1<number2)? number1 : number2;
-
-	                while(true){
-	                    if(number1 % i ==0 && number2 % i ==0){
-	                        System.out.println("GCD: "+i);
-	                        break;
-	                    }
-	                    i--;
-	                }
-	                int k = 1;
-	                while(k <= number1*number2){
-	                    if(k % number1 == 0 && k % number2 == 0){
-	                        System.out.println("LCM: "+k);
-	                        break;
-	                    }
-	                    k++;
-	                }
-
-	            }
-
-	        }
-
-
+        for(int j = 2 ; j <= 100 ;j++) {
+            int count = 0;
+            for (int i = 2; i <= j ; i++) {
+                if(j % i == 0 ){
+                count++;
+                }
+           }
+            if(count < 2){
+                System.out.print(j+" ");
+            }
+        }
+    }
+}
