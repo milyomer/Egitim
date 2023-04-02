73d4511f4ed0cac30102f9f10be31e7b214abc3b
root <root@DESKTOP-4N8IPC8>
2023-04-02 04:20:47 +0300

    Daha mukkemmel olan kod buldum. Orjinalin icine yorum satirina kaydedildi

--- Egitim/src/PrimeNumbers.java
+++ Egitim/src/PrimeNumbers.java
@@ -9,3 +9,21 @@ public class PrimeNumbers {
 				System.out.print(i+", " );
 	}		
 }
+
+
+//public class PrimeNumbers {
+//    public static void main(String[] args) {
+//
+//        for(int j = 2 ; j <= 100 ;j++) {
+//            int count = 0;
+//            for (int i = 2; i <= j ; i++) {
+//                if(j % i == 0 ){
+//                count++;
+//                }
+//           }
+//            if(count < 2){
+//                System.out.print(j+" ");
+//            }
+//        }
+//    }
+//}
