1544a8a218508a5a06f9b91f82209684759b855b
root <root@DESKTOP-4N8IPC8>
2023-04-02 15:13:55 +0300

    different code try

--- Egitim/src/deneme.java
+++ Egitim/src/deneme.java
@@ -1,16 +1,40 @@
-public class deneme {
-    public static void main(String[] args) {
+import java.util.*;
 
-        for(int j = 2 ; j <= 100 ;j++) {
-            int count = 0;
-            for (int i = 2; i <= j ; i++) {
-                if(j % i == 0 ){
-                count++;
-                }
-           }
-            if(count < 2){
-                System.out.print(j+" ");
+class deneme{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t =0;
+        do{
+            t=in.nextInt();
+        }while(t<0 || t>500);
+        
+            int a=0, b=0, n=0;
+        do{
+            for(int i=0;i<t;i++){
+                 a = in.nextInt();
+
+                 System.out.println("başarılı a");
+                 b = in.nextInt();
+
+                 System.out.println("başarılı b");
+                 n = in.nextInt();
+
+                 System.out.println("başarılı n");
+                 int k=1,sum=0;
+                 
+            	 sum=a+b;
+                 System.out.print((sum)+" ");
+                     for(int j=1; j<n;j++){
+                     k*=2;
+                     sum=(sum)+(k*b);
+                     System.out.print(sum+" ");
+                     }
+                     System.out.println();
+                 
             }
-        }
+        }while((a<0 || a>50) || (b<0 || b>50) || (n<1 || n>15));
+        
+        
+        
     }
 }
