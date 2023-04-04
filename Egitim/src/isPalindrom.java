34d7d83a428215baf1aba9c1138d41e6a2775b24
root <root@DESKTOP-4N8IPC8>
2023-04-05 00:17:04 +0300

    added another program inside quote

--- Egitim/src/isPalindrom.java
+++ Egitim/src/isPalindrom.java
@@ -17,3 +17,29 @@ public class isPalindrom {
 		System.out.println(isPalindrom(1565));
 	}
 }
+
+import java.util.Scanner;
+
+//public class isPalindrom {
+//    static void isPalindrome(int number) {
+//        int temp = number, reverseNumber = 0, lastNumber;
+//        while (temp != 0) {
+//            lastNumber = temp % 10;
+//            reverseNumber = (reverseNumber * 10) + lastNumber;
+//            temp /= 10;
+//        }
+//
+//        if (number == reverseNumber)
+//            System.out.println(number+" Palindrome number ");
+//        else
+//            System.out.println(number+" is not a Palindrome number ");
+//    }
+//
+//    public static void main(String args[]) {
+//        int k;
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Please enter a number: ");
+//        k = scanner.nextInt();
+//        isPalindrome(k);
+//    }
+//}
