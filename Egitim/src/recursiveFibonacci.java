f62fa473484075d30b4fa0dda7ff0eaf89150772
root <root@DESKTOP-4N8IPC8>
2023-04-05 01:19:42 +0300

    added recursiveFibonacci.java file

---
+++ Egitim/src/recursiveFibonacci.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class recursiveFibonacci {
+	static int fibonacci(int number) {
+		if (number ==2 || number== 1)
+			return 1;
+		return fibonacci(number-1)+ fibonacci(number-2);
+	}
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Lütfen bir sayı giriniz: ");
+		int number = input.nextInt();
+		System.out.println(fibonacci(number));
+	}
+}
