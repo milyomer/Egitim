ff9e510e9c93b0cb3f43177273837417a73f5c1b
gönlüzengin <gnlzengin@DESKTOP-DPIQHLF>
2023-04-09 02:42:31 +0300

    added Car.java and CarMain.java file

--- Egitim/src/deneme2.java
+++ Egitim/src/deneme2.java
@@ -1,33 +1,35 @@
-import java.util.*;
-public class deneme2{
-	class Sports{
-	  static String getName(){
-	        return "Generic Sports";
-	    }
-	    static void getNumberOfTeamMembers(){
-	        System.out.println( "Each team has n players in " + getName() );
-	    }
-	class Soccer extends Sports{
-	   static String getName(){
-	        return "Soccer Class";
-	    }
-	}
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("sdv: ");
-		int a = input.nextInt();
-		System.out.println(getName());
-		getNumberOfTeamMembers();
-	}
-	
-}
-}
+//import java.util.*;
+//public class deneme2{
+//	class Sports{
+//	  static String getName(){
+//	        return "Generic Sports";
+//	    }
+//	    static void getNumberOfTeamMembers(){
+//	        System.out.println( "Each team has n players in " + getName() );
+//	    }
+//	class Soccer extends Sports{
+//	   static String getName(){
+//	        return "Soccer Class";
+//	    }
+//	}
+//	public static void main(String[] args) {
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("sdv: ");
+//		int a = input.nextInt();
+//		System.out.println(getName());
+//		getNumberOfTeamMembers();
+//	}
+//	
+//}
+//}
+//
+//class Sports{
+//  static String getName(){
+//        return "Generic Sports";
+//    }
+//    static void getNumberOfTeamMembers(){
+//        System.out.println( "Each team has n players in " + getName() );
+//    }
+//}
 
-class Sports{
-  static String getName(){
-        return "Generic Sports";
-    }
-    static void getNumberOfTeamMembers(){
-        System.out.println( "Each team has n players in " + getName() );
-    }
-}
+class car
